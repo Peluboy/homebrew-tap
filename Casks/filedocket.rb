7273ec6ffc2858b7ba79086ad6b523907ea5d22e
Peluboy <imulep2104@gmail.com>
2026-08-27 06:54:22 -0500

    Update cask SHA256

--- Casks/filedocket.rb
+++ Casks/filedocket.rb
@@ -1,16 +1,12 @@
 cask "filedocket" do
   version "1.2.0"
-  sha256 "cf675c21f82321e14ea5b57d91f1b0746d0289f66be6c148084ef607a1d13e9a"
-
+  sha256 "55f5409fb2e846cc290f99f80751dc310bdd332e7efc17cd4429ef7eb57d336a"
   url "https://github.com/Peluboy/FileDocket/releases/download/v#{version}/FileDocket.dmg"
   name "FileDocket"
   desc "Automatic downloads folder organizer for macOS"
   homepage "https://peluboy.github.io/FileDocket"
-
   depends_on macos: ">= :monterey"
-
   app "FileDocket.app"
-
   zap trash: [
     "~/Library/LaunchAgents/com.filedocket.organizer.plist",
     "~/.file-organizer",

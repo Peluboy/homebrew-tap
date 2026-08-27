3d59e3ad807a95996a8917a48da021d9ad5baf08
Peluboy <imulep2104@gmail.com>
2026-08-27 04:15:47 -0500

    Update FileFlow with Lemon Squeezy integration

--- Casks/fileflow.rb
+++ Casks/fileflow.rb
@@ -1,6 +1,6 @@
 cask "fileflow" do
   version "1.2.0"
-  sha256 "32acc26bc6862ecdb3dd64fc3d1f79efefbf962e20aa18675e8506904572e8df"
+  sha256 "ec974cb85dc44f1efb368117f1b43c1f1a5296cd1e9dc09fa9a692220f9349dc"
 
   url "https://github.com/peluboy/FileFlow/releases/download/v#{version}/FileFlow.dmg"
   name "FileFlow"

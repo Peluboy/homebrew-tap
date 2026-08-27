8808bb1aade475a992f4b97bd04286196bd95276
Peluboy <imulep2104@gmail.com>
2026-08-27 01:14:31 -0500

    Add FileFlow cask formula v1.2.0
    Homebrew cask for installing FileFlow via:
      brew tap peluboy/homebrew-tap
      brew install --cask fileflow

---
+++ Casks/fileflow.rb
@@ -0,0 +1,18 @@
+cask "fileflow" do
+  version "1.2.0"
+  sha256 "730155aa13f269815189562d604ba11c4c5b6615ef9df26f2519914d86a0af05"
+
+  url "https://github.com/peluboy/FileFlow/releases/download/v#{version}/FileFlow.dmg"
+  name "FileFlow"
+  desc "macOS menu bar app that automatically organizes your Downloads folder"
+  homepage "https://github.com/peluboy/FileFlow"
+
+  depends_on macos: ">= :monterey"
+
+  app "FileFlow.app"
+
+  zap trash: [
+    "~/Library/Application Support/FileFlow",
+    "~/Library/Preferences/com.fileflow.plist",
+  ]
+end
